3e9cab5587efa07c93777c45fb976ab1b18ef33c
Do_Van_Thao <dothaoqthp@gmail.com>
2024-08-07 16:58:26 +0700

    chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
+  before_save :downcase_email
+  has_secure_password
+  def downcase_email
+    self.email.downcase!
+  end
+end
