b8dc8b2ef035d7c0ef0b9134f758bd64f208d4e9
Do_Van_Thao <dothaoqthp@gmail.com>
2024-08-08 08:26:00 +0700

    chapter 9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_07_092452) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_08_010229) do
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_07_092452) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
